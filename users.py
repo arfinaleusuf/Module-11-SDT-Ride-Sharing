87eecbdeeb859ab620c697b5278eb4881a863498
arfinaleusuf <arfinaleusuf@gmail.com>
2026-05-05 01:40:12 +0600

    driver class

--- users.py
+++ users.py
@@ -29,4 +29,20 @@ class Rider(User):
     def update_location(self, current_location):
         self.current_location = current_location
     
-    
+    def requst_ride(self, ride_sharing, destination):
+        pass
+    def show_current_ride(self):
+        print(self.current_ride)
+
+class Driver(User):
+    def __init__(self, name, email, nid, current_location):
+        super().__init__(name, email, nid)
+        self.current_location = current_location
+        self.walled = 0
+
+    def display_profile(self):
+        print(f'Driver Name{self.name}')
+
+    def accept_ride(self, ride):
+        #  accept korbo
+        pass
